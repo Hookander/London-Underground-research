2fef4459c75ac6b84aea9289df59f85f72261152
hookander <balthazar.martin123@gmail.com>
2025-01-24 19:47:06 +0100

    sync

--- model/model.py
+++ model/model.py
@@ -15,44 +15,45 @@ class Model(ModelClass):
         super().__init__()
         self.fc = nn.Linear(1, 1)
 
-    def create_data(self, syear:str, path: str) -> None:
+    def create_data(self, year:str, path: str) -> None:
         """
         Create the csv file for training wioth the following columns:
         - day
-        - month_cos
-        - month_sin -> to take into account the cyclical nature of the months (dec is close to jan)
+        - month
+        - year
         - tod_id
         - start_station_id
         - end_station_id
         - direction_id
         - hour
         - min (0, 15, 30 or 45), we are only interested in the quarter of the hour
+        - link_load
+        - output
 
         - taps ? We start without the taps
-
-        - link_load for the output of the model
 
         2019-2022 : training
         2023 : testing
         """
-        df = pd.DataFrame(columns=['day', 'month', 'tod_id', 'start_station_id', 'end_station_id', 'direction_id', 'hour', 'min', 'link_load'])
-        df.to_csv(path, index=False)
+        try:
+            df = pd.read_csv(path)
+        except:
+            print('Creating new file')
+            df = pd.DataFrame(columns=['day', 'month', 'year', 'tod_id', 'start_station_id', 'end_station_id', 'direction_id', 'hour', 'min', 'link_load', 'output'])
+            df.to_csv(path, index=False)
         stations = self.llh.get_all_stations()
         directions = ['EB', 'WB']
 
-        ntods_per_year = {}
-        for year in range(2019, 2024):
-            ntods_per_year[str(year)] = nb_days_per_tod(str(year))
-        for iyear in range(2019, 2023):
-            ll_dict = {}
-            year = str(iyear)
-            dates = get_dates_between('01/01/'+year, '31/12/'+year)
-            already_done_dates = (str(df['day']) + '/' + str(df['month']) + '/' + year).tolist()
-            dates_to_do = [date for date in dates if date not in already_done_dates]
-            for date in dates_to_do:
-                begin = time.time()
-                day_of_week = get_day_of_week(date)
-                type_of_day = get_type_of_day(day_of_week, include_friday=True)
+        ntods_per_year = nb_days_per_tod(year)
+        dates = get_dates_between('01/01/'+year, '31/12/'+year)
+        already_done_dates = (df['day'].astype(str) + '/' + df['month'].astype(str) + '/' + year).tolist()
+        print(already_done_dates)
+        dates_to_do = [date for date in dates if date not in already_done_dates]
+        print(dates_to_do)
+        for date in dates_to_do:
+            begin = time.time()
+            day_of_week = get_day_of_week(date)
+            type_of_day = get_type_of_day(day_of_week, include_friday=True)
 
             tod_id = self.tod_to_int(type_of_day)
 
@@ -73,17 +74,19 @@ class Model(ModelClass):
                                 output = avg_link_load * ntods_per_year[type_of_day]
                                 df = df._append({'day': day, 
                                                 'month' : month,
+                                                'year': year,
                                                 'tod_id': tod_id, 
                                                 'start_station_id': start_id, 
                                                 'end_station_id': end_id, 
                                                 'direction_id': dir_id, 
                                                 'hour': hour, 
-                                                'min': minute, 
+                                                'min': minute,
+                                                'link_load': avg_link_load,
                                                 'output': output}, ignore_index=True)
                     d2 = pd.read_csv(path)
                     df = pd.concat([d2, df])
                     df.to_csv(path, index=False)
-                    df = pd.DataFrame(columns=['day', 'month', 'tod_id', 'start_station_id', 'end_station_id', 'direction_id', 'hour', 'min', 'link_load'])
+                    df = pd.DataFrame(columns=['day', 'month', 'year', 'tod_id', 'start_station_id', 'end_station_id', 'direction_id', 'hour', 'min', 'link_load', 'output'])
 
             print(f'{date} done, time taken: {time.time() - begin}')
         
