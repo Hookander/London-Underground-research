1bf33f60aaf4d4e07e30de7bbcb38a3025e9d82a
Hookander <balthazar.martin123@gmail.com>
2025-02-25 13:55:01 +0000

    bug correction in the 2023 data, new dataset and reformating

--- data/Taps/taps.py
+++ data/Taps/taps.py
@@ -21,20 +21,28 @@ class tapsHandler():
         """
         df_full = pd.DataFrame(columns=['TravelDate','Station','EntryExit','TapCount', 'ServedBy'])
 
-        for year in range(2019, 2024):
+        for year in range(2019, 2023):
             df = pd.read_csv(f'./data/Taps/TAPS-daily-rail-station-entryexit-{year}.csv')
             df = df[df['ServedBy'].str.contains('Tube', na=False)]
+            df['TapCount'] = df['TapCount'].str.replace(',', '').astype(int)
             df_full = pd.concat([df_full, df], ignore_index=True)
+        # Handle the 2023 year since it is not fully in the TAPS file (lack of data)
+        df_2023 = pd.read_csv(f'./data/Taps/taps_alternate_2023-present.csv')
+        # TravelDate,Station,EntryExit,TapCount,ServedBy
+        df_2023 = df_2023.rename(columns={'TravelDateSk': 'TravelDate', 'StationName': 'Station', 'EntryOrExit': 'EntryExit', 'Count': 'TapCount'})
+        df_2023['ServedBy'] = 'Tube'
+        df_2023['TravelDate'] = pd.to_datetime(df_2023['TravelDate'], format='%Y%m%d').dt.strftime('%d/%m/%Y')
+        df_2023 = df_2023[df_2023['ServedBy'].str.contains('Tube', na=False)]
+        df_2023['Station'] = df_2023['Station'].str.replace(' LU', '')
+        df_full = pd.concat([df_full, df_2023], ignore_index=True)
         df_full.to_csv(self.path, index=False)
     
     def get_entries_exits(self, station:str, date:str) -> Dict[str, int]:
-        print(f"Getting entries and exits for {station} on {date}")
         """
             Returns the entries and exits for the given station and date
             date format : dd/mm/yyyy
         """
         if (station, date) in self.entries_exits:
-            print("Already in memory")
             return {'entries' : self.entries_exits[(station, date)]['entries'], 
                     'exits' : self.entries_exits[(station, date)]['exits']}
         else:
@@ -45,7 +53,7 @@ class tapsHandler():
             entries_df = filtered_df[filtered_df['EntryExit'] == 'Entry']
             exits_df = filtered_df[filtered_df['EntryExit'] == 'Exit']
             if len(entries_df['TapCount'].values) == 0 or len(exits_df['TapCount'].values) == 0:
-                print(f"No data for {date}, returning data for 7 days before : {station}")
+                print(f"No data for {date}, returning data for 7 days before (to have the same type of day (weekday or weekend)) : {station}")
                 # if for some reason there are no data for this day, we return the data
                 # for 7 days before (to have the same type of day (weekday or weekend))
                 previous_date = pd.to_datetime(date, format='%d/%m/%Y') - pd.DateOffset(days=7)
@@ -53,13 +61,13 @@ class tapsHandler():
                 #print(f"No data for {date}, returning data for {previous_date} : {station}")
                 return self.get_entries_exits(station, previous_date)
             else:
-                print(f"Data found for {date} : {station}")
+                
                 entries = entries_df['TapCount'].values[0]
                 exits = exits_df['TapCount'].values[0]
 
             #we want ints, now they are strings like 1,391
-            entries = int(entries.replace(',', ''))
-            exits = int(exits.replace(',', ''))
+            #entries = int(entries.replace(',', ''))
+            #exits = int(exits.replace(',', ''))
 
             self.entries_exits[(station, date)] = {'entries' : entries, 'exits' : exits}
 
